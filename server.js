2e18020d74296ec66f5e562b0dc9a45d0d0f557f
Kevin42127 <tyouxipindao@gmail.com>
2026-05-22 19:12:14 +0800

    Update to new secure YouTube API key

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = 3000;
 
 // YouTube API 配置
-const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY;
+const YOUTUBE_API_KEY = process.env.YOUTUBE_API_KEY || 'AIzaSyCOifIMSIRZV8b9y1AEvNUCb3_Ih9XvHRI';
 const YOUTUBE_API_BASE = 'https://www.googleapis.com/youtube/v3';
 
 // 中間件
